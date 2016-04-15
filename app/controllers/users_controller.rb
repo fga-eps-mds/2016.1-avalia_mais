cb0b3e97a54bfc2bc1a2fb32e5c2a8981bf26d71
Henrique <hlopes13@hotmail.com>
2016-04-15 17:13:42 -0300

    showing all validation erros, customizing validation messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
 			redirect_to :action => "show",:id => @user.id
 
 		else
-			flash[:error] = "Somethig is wrong"
 			render :new
 		end
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,28 @@
 class User < ActiveRecord::Base
 	
-	validates :name, presence: true, length: { minimum: 3, maximum: 50}
-	validates :email, presence: true, length: { maximum: 50}, uniqueness: true
-	validates :password, presence: true, length: { minimum: 4, maximum: 16}
-	validates :dateBirthday, presence: true
-	validates :gender, presence: true
-	validates :login, presence: true, length: { minimum: 4, maximum: 50}, uniqueness: true
+	#name
+	validates_length_of :name,
+  		:within => 3..50,
+  		:too_short => 'Nome tem que ter no minimo 3 caracters',
+  		:too_long => 'Nome tem que ter no maximo 50 caracters'
+
+	#email
+	validates :email, :presence => { :message => 'Email nao pode ser vazio' }, length: { maximum: 50}, uniqueness: true
+
+	#password
+	validates_length_of :password,
+  		:within => 4..16,
+  		:too_short => 'Senha tem que ter com no minimo 4 caracters',
+  		:too_long => 'Senha tem que tercom no maximo 16 caracters'
+
+  	#gender
+	validates :gender, :presence => { :message => 'Escolha seu genero' }
+
+	#login
+	validates_length_of :login,
+  		:within => 4..50,
+  		:too_short => 'Apelido com no minimo 3 caracters',
+  		:too_long => 'Apelido com no maximo 50 caracters'
 
 end
 
