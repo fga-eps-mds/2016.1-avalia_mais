1113b182e2c91a8e11df41b620f5c531319005f7
GesielFreitas <gesiel.was.f@gmail.com>
2016-04-15 21:52:42 -0300

    homepage avalia mais

---
+++ app/controllers/welcone_controller.rb
@@ -0,0 +1,4 @@
+class WelconeController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcone/index'
+
   get 'users/new'
   get 'users/show'
   get 'users/edit'
   get 'users/destroy'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
