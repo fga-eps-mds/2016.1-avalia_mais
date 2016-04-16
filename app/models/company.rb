204ef97bac2301ac7920829e9a579a2eb99712f2
YOUR NAMEThiago <thiago.cmoreira@yahoo.com.br>
2016-04-16 15:26:57 -0300

    Update companies/new.html.erb

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,16 +4,16 @@ class Company < ActiveRecord::Base
 	has_attached_file :logo, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
 
-	validates :name, 
-		presence: { :message => "Informe o nome da empresa." },
-		uniqueness: { :message => "Uma empresa já foi cadastrada com esse nome."},	
-		length: { minimum: 2, maximum: 50 } 
+	# validates :name, 
+	# 	presence: { :message => "Informe o nome da empresa." },
+	# 	uniqueness: { :message => "Uma empresa já foi cadastrada com esse nome."},	
+	# 	length: { minimum: 2, maximum: 50 } 
 		
 		
 
 
-	validates :address,
-		length: { minimum: 3, maximum: 80 }
+	# validates :address,
+	# 	length: { minimum: 3, maximum: 80 }
 
-	validates :description, length: { maximum: 500 }
+	# validates :description, length: { maximum: 500 }
 end
