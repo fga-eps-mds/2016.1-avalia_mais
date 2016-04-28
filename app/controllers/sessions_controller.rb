2cf15c3fb766ef99c93eb3335510e805978d8ecd
Julio Xavier <julioxavierr@live.com>
2016-04-27 21:52:08 -0300

    User can log in using email too

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(login: params[:session][:login])
+		user = find_user
 
 		if user && user.authenticate(params[:session][:password])
 			log_in user
-			redirect_to user_show_path(user), notice: "Login realizado com sucesso"
+			redirect_to user_show_path(user), notice: 'Login realizado com sucesso'
 		else 
 			flash.now[:notice] = 'O login e/ou a senha inválidos'
 			render 'new'
@@ -17,7 +17,12 @@ class SessionsController < ApplicationController
 
 	def destroy
 		session[:user_id] = nil
-		redirect_to  home_path, notice: "Você saiu"
+		flash.now[:notice] = 'Logout efetuado com sucesso'
+		redirect_to home_path
+	end
+
+	def find_user
+		User.find_by(login: params[:session][:login]) || User.find_by(email: params[:session][:login])
 	end
 
 end
