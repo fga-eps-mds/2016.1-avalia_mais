7c328383b4580b25ae84aaa65afa852ea78e8fd6
Thiago moreira <thiago.cmoreira@yahoo.com.br>
2016-05-02 09:53:25 -0300

    Fix self.save_data of parser and add css to companies/search

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -3,7 +3,7 @@ require 'csv'
 class Parser < ActiveRecord::Base
 	#file = '/public/csv/2016-02.csv'
 	def self.save_data(file)
-		CSV.foreach(file, :col_sep => ';', :headers =>true, :encoding => 'windows-1251:utf-8') do |row|
+		CSV.foreach(file, :col_sep => ';', :headers =>true, :encoding => ':utf-8') do |row|
 			segment_record = Segment.find_or_create_by(:name => row[12])
 			company_record = segment_record.companies.find_or_create_by(:name => row[11])
 			uf_record = Uf.find_or_create_by(:name => row[1])
@@ -11,10 +11,6 @@ class Parser < ActiveRecord::Base
 				:resolved => row[21])
 
 			#evaluation_record.update_attributes(:uf_id => row[1])
-
-
-
-
 		end
 	end
 end
