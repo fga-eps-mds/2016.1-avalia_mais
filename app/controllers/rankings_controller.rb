85c3f8d9389a2190ec192e5fcf9b930aaf84db63
vitorbertulucci <vitorbertulucci@hotmail.com>
2016-05-26 17:45:18 -0300

    "Improved view of ranking by response time. Start to do ranking by segments(response time)"
    Signed-off-by: vitorbertulucci <vitorbertulucci@hotmail.com>
    Signed-off-by: Thiago Moreira <thiago.cmoreira@yahoo.com.br>

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,8 +1,16 @@
 class RankingsController < ApplicationController
 
 	def response_time_all
-		@companies = Company.all.order(:response_time)
+		@companies = Company.all.order(:response_time).paginate(:page => params[:page], :per_page => 20)
 
+	end
+	def response_time_segment
+		@segments = Segment.all
+		@companies = Company.all.order(:response_time)
+	end
+	
+	def segment_selected
+		@companies = Company.where(:segment_id => :segment).order(:response_time).paginate(:page => params[:page], :per_page => 20)
 	end
 
 	def grade_all
