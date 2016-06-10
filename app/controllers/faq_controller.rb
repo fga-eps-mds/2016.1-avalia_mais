bc1e2f70db040269c3d76bd292f3c3af346cf2bc
GesielFreitas <gesiel.was.f@gmail.com>
2016-06-09 22:16:01 -0300

    correction in question validades and view faq
    Signed-off-by: GesielFreitas <gesiel.was.f@gmail.com>
    Signed-off-by: hlopes13 <hlopes13@hotmail.com>

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -6,7 +6,6 @@ class FaqController < ApplicationController
 
   def create
     @faq = Faq.new(faq_params)
-    debugger
     if @faq.save  
         
         @company = Company.find(params[:faq][:company_id])
