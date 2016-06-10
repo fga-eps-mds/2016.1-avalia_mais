4e4c96aff91c82d2c3d250d2a833f6dfce2df0b2
Julio Xavier <julioxavierr@live.com>
2016-06-10 20:31:38 -0300

    Spec for denunciations controller
    Signed-off-by: Julio Xavier <julioxavierr@live.com>
    Signed-off-by: Thiago Moreira <thiago.cmoreira@yahoo.com.br>

--- app/controllers/denunciations_controller.rb
+++ app/controllers/denunciations_controller.rb
@@ -1,6 +1,7 @@
 class DenunciationsController < ApplicationController
 	def create
-		if Denunciation.create(topic_id: params[:session][:topic_id], user_id: params[:session][:user_id])
+		@denunciation = Denunciation.create(topic_id: params[:session][:topic_id], user_id: params[:session][:user_id])
+		if @denunciation
 			redirect_to Topic.find(params[:session][:topic_id])
 		else
 			# error
