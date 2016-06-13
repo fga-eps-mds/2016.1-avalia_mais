3ae5bd39b8a1f0fa211685e641b4be0b5b389296
vitorbertulucci <vitorbertulucci@hotmail.com>
2016-06-13 13:12:27 -0300

    "Back end and and view already done. Minor changes needed."
    Signed-off-by: vitorbertulucci <vitorbertulucci@hotmail.com>
    Signed-off-by: GesielFreitas <gesiel.was.f@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get  '/company/:id' => 'companies#show', :as => 'company'
   get '/company/:search' => 'companies#search', :as => 'company_search'
   post '/company/new' => 'companies#create', :as => 'create_company'
+  get '/company/edit/:id' => 'companies#edit', :as => 'edit_company'
+  post '/company/update' => 'companies#update', :as => 'update_company'
 
   #users
   get   '/users/new'      => 'users#new'
