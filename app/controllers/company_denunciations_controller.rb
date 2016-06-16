127321741b5bcd0b41410b189ee9f6562315965a
Thiago moreira <thiago.cmoreira@yahoo.com.br>
2016-06-16 16:41:48 -0300

    Added unit tests to company denunciations
    Signed-off-by: Thiago moreira <thiago.cmoreira@yahoo.com.br>
    Signed-off-by: Henrique Lopes <hlopes13@hotmail.com>

--- app/controllers/company_denunciations_controller.rb
+++ app/controllers/company_denunciations_controller.rb
@@ -1,6 +1,6 @@
 class CompanyDenunciationsController < ApplicationController
 	def create
-		@company_denunciation = CompanyDenunciation.create(company_id: params[:session][:company_id], user_id: params[:session][:user_id])
+		@company_denunciation = CompanyDenunciation.create(company_id: params[:session][:company_id], user_id: params[:session][:user_id], description: params[:session][:description])
 		if @company_denunciation
 			redirect_to Company.find(params[:session][:company_id])
 			flash[:notice] = 'Sua denúncia foi efetuada com sucesso!'
