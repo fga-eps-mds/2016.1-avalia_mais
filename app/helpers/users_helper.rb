598640c5554c523b40d4cc79a967566382f37891
Julio Xavier <julioxavierr@live.com>
2016-06-20 18:44:23 -0300

    Optimizing and removing unnecessary controller methods.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,12 +4,4 @@ module UsersHelper
     	date.strftime("%d/%m/%Y ")
   	end
 
-  	def user_admin?
-  		@user = current_user
-		@user.admin == true
-	end
-
-	def redirect_not_admin
-		redirect_to home_path
-	end
 end
